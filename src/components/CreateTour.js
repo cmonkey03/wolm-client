e3b605eeed3d40721ab11146141833c488f67cf0
cmonkey03 <russellpbrown@gmail.com>
2018-10-15 10:30:19 -0400

    completed CreateTour form

--- src/components/CreateTour.js
+++ src/components/CreateTour.js
@@ -1,37 +1,58 @@
 import React from 'react';
-import { Button, Form, Input } from 'semantic-ui-react';
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+import { Button, Form, Input, Label } from 'semantic-ui-react';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class CreateTour extends React.Component {
   state = {
-    startTimeInput: "",
-    endTimeInput: "",
-    price: null
+    startTime: moment(),
+    endTime: moment(),
+    price: 0
   }
 
+  handleStartTimeChange = (date) => this.setState({startTime: date})
+  handleEndTimeChange = (date) => this.setState({endTime: date})
+  handlePriceChange = (event) => this.setState({price: event.target.value})
+
   render() {
-    console.log(this.state)
     return (
       <React.Fragment>
         <h1>Create a Tour</h1>
         <Form>
-          <Form.Field
-              control={Input}
-              label='Username'
-              placeholder="Username"
-              type="datetime"
-              maxLength='16'
-              value={this.state.startTimeInput}
-          />
           <Form.Field>
-            <label>Password
-            <input  placeholder="Password"
-                    type="password"
-                    value={this.state.endTimeInput}
-                    maxLength='32'
-                    />
-                </label>
+            <Label>Start Time</Label>
+            <DatePicker selected={this.state.startTime}
+                        showTimeSelect
+                        dateFormat="LLL"
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        onChange={this.handleStartTimeChange}
+                        />
           </Form.Field>
-          <Button type='submit'>Login</Button>
+          <Form.Field>
+            <Label>End Time</Label>
+            <DatePicker selected={this.state.endTime}
+                        showTimeSelect
+                        dateFormat="LLL"
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        onChange={this.handleEndTimeChange}
+                        />
+          </Form.Field>
+          <Form.Field>
+            <Label>Price</Label>
+            <Input labelPosition='right' type='text' placeholder='Amount'>
+              <Label basic>$</Label>
+              <input  type='number'
+                      min='0'
+                      value={this.state.price}
+                      onChange={this.handlePriceChange}
+                      />
+              <Label>.00</Label>
+            </Input>
+          </Form.Field>
+          <Button type='submit'>Create Tour</Button>
         </Form>
       </React.Fragment>
     )
