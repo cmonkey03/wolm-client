023c2eaef22d2d9cfe5904ff06bc6a4b38fd729a
cmonkey03 <russellpbrown@gmail.com>
2018-10-15 10:55:21 -0400

    Added partially complete onSubmit function & minDate to fields

--- src/components/CreateTour.js
+++ src/components/CreateTour.js
@@ -1,29 +1,57 @@
 import React from 'react';
+import ApiAdapter from '../adapter';
 import moment from 'moment';
 import DatePicker from 'react-datepicker';
 import { Button, Form, Input, Label } from 'semantic-ui-react';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export default class CreateTour extends React.Component {
-  state = {
-    startTime: moment(),
-    endTime: moment(),
-    price: 0
+  constructor(props) {
+    super(props)
+    this.ApiAdapter = new ApiAdapter()
+    this.state = {
+      startTime: moment(),
+      endTime: moment(),
+      price: 0,
+      errors: null
+    }
   }
 
   handleStartTimeChange = (date) => this.setState({startTime: date})
   handleEndTimeChange = (date) => this.setState({endTime: date})
   handlePriceChange = (event) => this.setState({price: event.target.value})
 
+  handleTourSubmit = (e) => {
+    const tourObj = {
+      start_time: this.state.startTime,
+      end_time: this.state.endTime,
+      price: this.state.price
+    }
+
+    this.ApiAdapter.postTour(tourObj).then(r=>{
+      if (r.errors) {
+        this.setState({errors: r.errors}, () => console.log(this.state))
+      } else {
+        this.setState({
+          startTime: moment(),
+          endTime: moment(),
+          price: 0,
+          errors: null
+        })
+      }
+    })
+  }
+
   render() {
     return (
       <React.Fragment>
         <h1>Create a Tour</h1>
-        <Form>
+        <Form onSubmit={this.handleTourSubmit}>
           <Form.Field>
             <Label>Start Time</Label>
             <DatePicker selected={this.state.startTime}
                         showTimeSelect
+                        minDate={moment()}
                         dateFormat="LLL"
                         timeFormat="HH:mm"
                         timeIntervals={15}
@@ -34,6 +62,7 @@ export default class CreateTour extends React.Component {
             <Label>End Time</Label>
             <DatePicker selected={this.state.endTime}
                         showTimeSelect
+                        minDate={this.state.endTime}
                         dateFormat="LLL"
                         timeFormat="HH:mm"
                         timeIntervals={15}
