1e61b1ae06cc85d00904fb39b2ca07b8a760af87
cmonkey03 <russellpbrown@gmail.com>
2018-10-22 20:42:07 -0400

    Error message display added to Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Redirect } from 'react-router';
-import { Button, Form, Input, Segment } from 'semantic-ui-react';
+import { Button, Form, Input, Message, Segment } from 'semantic-ui-react';
 import { loginUser }from '../actions/user';
 
 class Login extends React.Component {
@@ -26,8 +26,8 @@ class Login extends React.Component {
           loading={this.props.authenticatingUser}
           error={this.props.failedLogin}
         >
-          <Form.Field
-              control={Input}
+          <Message error header={this.props.failedLogin ? this.props.error : null} />
+          <Form.Input
               label='Username'
               placeholder='Username'
               name='username'
@@ -36,15 +36,14 @@ class Login extends React.Component {
               value={this.state.username}
               onChange={this.handleChange}
           />
-          <Form.Field
-              control={Input}
-              label='Password'
-              placeholder='Password'
-              name='password'
-              type='password'
-              value={this.state.password}
-              onChange={this.handleChange}
-              maxLength='32'
+          <Form.Input
+                label='Password'
+                placeholder='Password'
+                name='password'
+                type='password'
+                value={this.state.password}
+                onChange={this.handleChange}
+                maxLength='32'
           />
           <Button type='submit'>Login</Button>
         </Form>
