f8dd03beeb79e2592f43a65fbbc713fc53b2454b
cmonkey03 <russellpbrown@gmail.com>
2018-10-23 19:12:15 -0400

    Updated color-changing NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,76 +9,79 @@ const colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'brown']
 class NavBar extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      active: ""
-    }
+    this.state = { active: "Login" }
   }
 
   handleClick = (e, { name }) => this.setState({ active: name })
 
   render() {
+    const {active} = this.state
+    console.log(this.state)
     return(
-        <div>
-          <Menu inverted>
-            {!this.props.loggedIn &&
-              <Menu.Item
-                as={NavLink}
-                to="/login"
-                onClick={this.handleClick}
-                name="Login"
-                color={colors[0]}
-                />
-            }
-            {!this.props.loggedIn &&
-              <Menu.Item
-                as={NavLink}
-                to="/signup"
-                onClick={this.handleClick}
-                name="Signup"
-                color={colors[1]}
+        <Menu inverted>
+          {!this.props.loggedIn &&
+            <Menu.Item
+              as={NavLink}
+              to="/signup"
+              name="Signup"
+              active={active==="Signup"}
+              color={colors[4]}
+              onClick={this.handleClick}
+            />}
+          {!this.props.loggedIn &&
+            <Menu.Item
+              as={NavLink}
+              to="/login"
+              name="Login"
+              active={active==="Login"}
+              color={colors[0]}
+              onClick={this.handleClick}
+              />
+          }
+          {this.props.loggedIn &&
+            <Menu.Item
+              as={NavLink}
+              to="/reservations"
+              name="Reservations"
+              active={active==="Reservations"}
+              color={colors[0]}
+              onClick={this.handleClick}
+            />}
+          {this.props.loggedIn && this.props.user.admin &&
+            <Menu.Item
+              as={NavLink}
+              to="/admin"
+              name="Administrator"
+              active={active==="Administrator"}
+              color={colors[4]}
+              onClick={this.handleClick}
+            />}
+          {this.props.loggedIn && this.props.user.admin &&
+            <Menu.Item
+              as={NavLink}
+              to="/new-tour"
+              name="Create Tour"
+              active={active==="Create Tour"}
+              color={colors[5]}
+              onClick={this.handleClick}
+            />}
+          {this.props.loggedIn &&
+            <Menu.Item
+              as={NavLink}
+              to="/edit-profile"
+              name="Edit Profile"
+              active={active==="Edit Profile"}
+              color={colors[2]}
+              onClick={this.handleClick}
+            />}
+          { this.props.loggedIn &&
+            <Menu.Item
+              as={NavLink}
+              to="/login"
+              onClick={this.props.logoutUser}
+              name={`Logout ${this.props.user.username}`}
               />}
-            {this.props.loggedIn &&
-              <Menu.Item
-                as={NavLink}
-                to="/reservations"
-                onClick={this.handleClick}
-                name="Reservations"
-                color={colors[3]}
-              />}
-            {this.props.loggedIn && this.props.user.admin &&
-              <Menu.Item
-                as={NavLink}
-                to="/admin"
-                onClick={this.handleClick}
-                name="Administrator"
-                color={colors[4]}
-              />}
-            {this.props.loggedIn && this.props.user.admin &&
-              <Menu.Item
-                as={NavLink}
-                to="/new-tour"
-                onClick={this.handleClick}
-                name="Create Tour"
-                color={colors[5]}
-              />}
-            {this.props.loggedIn &&
-              <Menu.Item
-                as={NavLink}
-                to="/edit-profile"
-                onClick={this.handleClick}
-                name="Edit Profile"
-                color={colors[2]}
-              />}
-              { this.props.loggedIn &&
-                <Menu.Item
-                  as={NavLink}
-                  to="/login"
-                  onClick={this.props.logoutUser}
-                  name={`Logout ${this.props.user.username}`}
-                  />
-              }
-          </Menu>
-        </div>
+        </Menu>
       )
   }
 }
