d8cd99bf71f310295d8775e9caaa45294ae27ea8
cmonkey03 <russellpbrown@gmail.com>
2018-10-25 14:17:18 -0400

    Added text to TourInfo

--- src/components/TourInfo.js
+++ src/components/TourInfo.js
@@ -25,6 +25,17 @@ const TourInfo = () => (
       utroque quo. Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi
       convenire mnesarchum eu per, quas minimum postulant per id.
     </p>
+    <p>
+      Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi
+      nulla ex duo. Vis sint solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix
+      fabulas oporteat, ei quo vide phaedrum, vim vivendum maiestatis in.
+    </p>
+    <p>
+      Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem,
+      est autem aliquip detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum
+      ea laoreet recteque electram, eos choro alterum definiebas in. Vim dolorum definiebas an. Mei
+      ex natum rebum iisque.
+    </p>
   </Segment>
 )
 
