2cb656e6ad23194c0187364a6a5fbad400cf5e3a
github username <github email address>
2018-11-26 19:29:03 -0500

    Changed NavLink to Link and replaced active prop to links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 import { logoutUser } from '../actions/user';
 
@@ -10,12 +10,12 @@ const colors = ['teal', 'olive', 'brown', 'yellow']
 //in order to indicate an active color
 
 class NavBar extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   // this.state = {}
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-  // handleClick = (e, { name }) => this.setState({ activeItem: name })
+  handleClick = (e, { name }) => this.setState({ activeItem: name })
 
   handleMenuSize = () => {
     if (this.props.loggedIn && this.props.user.admin) {
@@ -28,76 +28,91 @@ class NavBar extends Component {
   }
 
   render() {
-    // const {activeItem} = this.state
-    // console.log(this.state)
+    const {activeItem} = this.state
+    console.log(this.state, this.props)
     return(
         <Menu widths={this.handleMenuSize()} inverted stackable>
             <Menu.Item
-              as={NavLink}
-              exact
+              header
+              as={Link}
               to="/tours"
+              exact
               name="tours"
-              // active={activeItem==="tours"}
+              active={activeItem==="tours"}
               color={colors[2]}
               onClick={this.handleClick}
             />
           {!this.props.loggedIn &&
             <Menu.Item
-              as={NavLink}
-              exact
+              header
+              as={Link}
               to="/login"
+              exact
               name="login"
+              active={activeItem==="login"}
               color={colors[0]}
               onClick={this.handleClick}
             />}
           {!this.props.loggedIn &&
             <Menu.Item
-              as={NavLink}
-              exact
+              header
+              as={Link}
               to="/signup"
+              exact
               name="signup"
+              active={activeItem==="signup"}
               color={colors[1]}
               onClick={this.handleClick}
             />}
           {this.props.loggedIn &&
             <Menu.Item
-              as={NavLink}
-              exact
+              header
+              as={Link}
               to="/reservations"
+              exact
               name="reservations"
+              active={activeItem==="reservations"}
               color={colors[0]}
               onClick={this.handleClick}
             />}
           {this.props.loggedIn && this.props.user.admin &&
             <Menu.Item
-              as={NavLink}
-              exact
+              header
+              as={Link}
               to="/admin"
+              exact
               name="administrator"
+              active={activeItem==="administrator"}
               color={colors[1]}
               onClick={this.handleClick}
             />}
           {this.props.loggedIn && this.props.user.admin &&
             <Menu.Item
-              as={NavLink}
-              exact
+              header
+              as={Link}
               to="/new-tour"
-              name="create Tour"
+              exact
+              name="create tour"
+              active={activeItem==="create tour"}
               color={colors[2]}
               onClick={this.handleClick}
             />}
           {this.props.loggedIn &&
             <Menu.Item
-              as={NavLink}
+              header
+              as={Link}
               to="/edit-profile"
-              name="edit Profile"
+              exact
+              name="edit profile"
+              active={activeItem==="edit profile"}
               color={colors[3]}
               onClick={this.handleClick}
             />}
           {this.props.loggedIn &&
             <Menu.Item
-              as={NavLink}
+              as={Link}
               to="/login"
+              color={colors[3]}
               onClick={this.props.logoutUser}
               name={`Logout ${this.props.user.username}`}
               />}
