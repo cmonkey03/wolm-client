ada52303860b41616c76fdafb6544ab2e3c6bc09
github username <github email address>
2018-12-22 20:47:46 -0500

    Changed homepage components to non-semantic

--- src/containers/HomepageLayout.js
+++ src/containers/HomepageLayout.js
@@ -1,36 +1,23 @@
-import React, { Component } from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import React, { Component, Fragment } from 'react';
 import NavBar from '../components/NavBar';
 
-
-
-  // <figure className="wolm-intro">
-  //   <p className='hero-text'>Website</p>
-  //   <p className='hero-text'>Of</p>
-  //   <p className='hero-text'>Lower</p>
-  //   <p className='hero-text'>Manhattan</p>
-  //   </figure>
-
 class HomepageLayout extends Component {
-
   render() {
     return (
-      <Grid>
-      <Grid.Row columns='1'>
-      <Grid.Column>
-      <Image
-      alt="1892 NY Harbor"
-      src="/1892-nyc-currier-ives-1920-1080.jpg"
-      fluid
-      />
-      </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-      <Grid.Column>
-      <NavBar />
-      </Grid.Column>
-      </Grid.Row>
-      </Grid>
+      <Fragment>
+        <img
+          className="home-image"
+          alt="1892 NY Harbor"
+          src="/1892-nyc-currier-ives-1920-1080.jpg"
+        />
+        <div>
+          <p className='wolm-intro'>Website</p>
+          <p className='wolm-intro'>Of</p>
+          <p className='wolm-intro'>Lower</p>
+          <p className='wolm-intro'>Manhattan</p>
+        </div>
+        <NavBar />
+      </Fragment>
     )
   }
 }
