15038d44a441320fce6e47f65da95c57639c7a30
github username <github email address>
2019-01-03 12:46:11 -0500

    Duplicated HomepageLayout

---
+++ src/containers/HomepageLayout1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import NavBar from '../components/NavBar';
+
+const HomepageLayout = () => (
+  <React.Fragment>
+    <Container className='home' fluid>
+      <p className='home-text'>Website</p>
+      <p className='home-text'>Of</p>
+      <p className='home-text'>Lower</p>
+      <p className='home-text'>Manhattan</p>
+    </Container>
+    <NavBar />
+  </React.Fragment>
+)
+
+
+export default HomepageLayout;
