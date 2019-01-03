e8ee75de48ceb83833fe795ef03d356042c46dec
github username <github email address>
2019-01-03 15:21:31 -0500

    Added homepage button

--- src/containers/HomepageLayout.js
+++ src/containers/HomepageLayout.js
@@ -45,10 +45,10 @@ const HomepageHeading = ({ mobile }) => (
         marginTop: mobile ? '0.5em' : '1.5em',
       }}
     />
-    {/*<Button primary size='huge'>
-      Get Started
-      <Icon name='right arrow' />
-    </Button>*/}
+    <Button primary size='huge'>
+      Begin
+      {/*<Icon name='right arrow' />*/}
+    </Button>
   </Container>
 )
 
