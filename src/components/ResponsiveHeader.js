6e63baf12206cda4282f1fc85a1f013367025741
github username <github email address>
2019-01-08 17:12:41 -0500

    Moved HomepageHeading to new file

--- src/components/ResponsiveHeader.js
+++ src/components/ResponsiveHeader.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { withRouter } from 'react-router';
+import HomepageHeading from './HomepageHeading';
 import {
   Button,
   Container,
@@ -12,46 +13,11 @@ import {
   Sidebar,
   Visibility,
 } from 'semantic-ui-react'
-// import NavBar from '../components/NavBar';
 
 /* eslint-disable react/no-multi-comp */
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
 * such things.
 */
-
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      as='h1'
-      content='WOLM'
-      inverted
-      style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
-      }}
-    />
-    <Header
-      as='h2'
-      content='Tour like a local . . . in 1755.'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button primary size='huge'>
-      Begin
-      {/*<Icon name='right arrow' />*/}
-    </Button>
-  </Container>
-)
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
 
 /* Heads up!
  * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
