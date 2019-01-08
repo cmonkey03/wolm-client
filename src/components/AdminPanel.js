56c393b320fd550328d51ff3cca7c0bafe974045
github username <github email address>
2019-01-08 16:41:14 -0500

    Removed ApiData load from App

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import withAuth from '../hocs/withAuth';
+import store from '../store';
+import { connect } from 'react-redux';
 import AllUsers from './AllUsers.js';
 import AllTours from './AllTours.js';
 import AllReservations from './AllReservations.js';
 import { Header, Icon } from 'semantic-ui-react';
+import { LOAD_API_DATA } from '../types';
+import ApiAdapter from '../adapter';
 
-const AdminPanel = (props) => {
-  return(
-    <React.Fragment>
-      <Header as='h2' icon textAlign='center'>
-      <Icon name='settings'/>
-      Administrator Panel
-      <Header.Subheader>Manage tours, users and reservations.</Header.Subheader>
-    </Header>
-      <AllUsers />
-      <AllTours />
-      <AllReservations />
-    </React.Fragment>
-  )
+class AdminPanel extends Component {
+  constructor(props) {
+    super(props)
+    this.ApiAdapter = new ApiAdapter()
+  }
+
+  componentDidMount() {
+    this.ApiAdapter.getApiData().then(initialAppState=>{
+      const payload = {
+        users: initialAppState[0],
+        tours: initialAppState[1],
+        reservations: initialAppState[2]
+      }
+
+      store.dispatch({
+        type: LOAD_API_DATA,
+        payload
+      })
+    })
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <Header as='h2' icon textAlign='center'>
+        <Icon name='settings'/>
+        Administrator Panel
+        <Header.Subheader>Manage tours, users and reservations.</Header.Subheader>
+      </Header>
+        <AllUsers />
+        <AllTours />
+        <AllReservations />
+      </React.Fragment>
+    )
+  }
 }
 
-export default withAuth(AdminPanel);
+const mapStateToProps = ({users, tours, reservations}) => {
+  return {
+    users,
+    tours,
+    reservations
+  }
+}
+
+export default withAuth(connect(mapStateToProps)(AdminPanel));
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import '../App.css';
-import store from '../store';
-import ApiAdapter from '../adapter';
-import { connect } from 'react-redux';
-import { LOAD_API_DATA } from '../types';
 import { Redirect, withRouter } from 'react-router';
 import { Route, Switch } from 'react-router-dom';
 import HomepageLayout from '../components/HomepageLayout';
@@ -20,25 +16,6 @@ import { Container } from 'semantic-ui-react';
 import ResponsiveHeader from '../components/ResponsiveHeader';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.ApiAdapter = new ApiAdapter()
-  }
-
-  componentDidMount() {
-    this.ApiAdapter.getApiData().then(initialAppState=>{
-      const payload = {
-        users: initialAppState[0],
-        tours: initialAppState[1],
-        reservations: initialAppState[2]
-      }
-
-      store.dispatch({
-        type: LOAD_API_DATA,
-        payload
-      })
-    })
-  }
 
   // renderHeader() {
   //   return (
@@ -85,12 +62,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({users, tours, reservations}) => {
-  return {
-    users,
-    tours,
-    reservations
-  }
-}
-
-export default withRouter(connect(mapStateToProps)(App));
+export default withRouter(App);
