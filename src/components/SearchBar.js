cdedd155fc6d27628336c455b59f74629da09561
github username <github email address>
2019-02-01 16:44:20 -0500

    Added search render

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,5 +7,14 @@ import {
 } from 'semantic-ui-react'
 
 export default class SearchBar extends Component {
-
+  render() {
+     <Search
+            {/*loading={isLoading}
+            onResultSelect={this.handleResultSelect}
+            onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
+            results={results}
+            value={value}
+            {...this.props}*/}
+          />
+  }
 }
