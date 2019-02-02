044432d0c7f0b7fd5ebbe42a0f7c743be58bd7ec
github username <github email address>
2019-02-02 17:04:24 -0500

    Added createTours method

--- src/components/MakeReservation.js
+++ src/components/MakeReservation.js
@@ -22,6 +22,10 @@ class MakeReservation extends React.Component {
     this.props.unmountMakeReservation()
   }
 
+  currentTours(tours) {
+
+  }
+
   findTour(tour_id, tours) {
     const tour = tours.find((tour) => tour.id === tour_id)
     return tour
