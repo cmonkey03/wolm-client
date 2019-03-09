0390084cf8eebe2505c4bb123a82db106b660e4b
Russell Brown <info@russellpatrickbrown.com>
2019-03-09 16:41:47 -0500

    Updated imports

--- src/components/Reservations2.js
+++ src/components/Reservations2.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import withAuth from '../hocs/withAuth';
 import { cancelReservation } from '../actions/reservation';
 import { loadTours } from '../actions/tour';
 import { fetchCurrentUser } from '../actions/user';
 import {
-  Container,
   Grid,
   Image
 } from 'semantic-ui-react';
@@ -15,7 +15,7 @@ class Reservations2 extends React.Component  {
   reservationTile = (reservations) => {
     return (reservations.reduce((accum, reservation) => {
       accum.push(
-        <Grid.Column>
+        <Grid.Column key={reservation.id}>
           <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
         </Grid.Column>
       )
